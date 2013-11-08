21368a0366e73ebc9dff0462259a4ccefd5ed3f5
phppirate <sam@podlogar.com>
2013-11-08 16:26:01 -0600

    added users/authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  class ApplicationController < ActionController::Base
+  USER, PASSWORD = User.last.email, User.last.password
+ 
+  before_filter :authentication_check
+
+ 
+  private
+    def authentication_check
+      authenticate_or_request_with_http_basic do |user, password|
+        user == USER && password == PASSWORD
+      end
+    end
+end
 end
